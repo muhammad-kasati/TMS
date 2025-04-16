d01a4f103b6741bfc6d21b10d23f1204eb3314dc
Mohammad_Khasati <126200306+muhammad-kasati@users.noreply.github.com>
2025-04-16 23:37:56 +0300

    Update firebase.ts

--- firebase.ts
+++ firebase.ts
@@ -3,12 +3,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey:"AIzaSyCccUR3wUM21ZWvsP-iEoHm25ZRo56Ei_Q",
-  authDomain: "mern-blog-9fa94.firebaseapp.com",
-  projectId: "mern-blog-9fa94",
-  storageBucket: "mern-blog-9fa94.appspot.com",
-  messagingSenderId: "463555536834",
-  appId: "1:463555536834:web:2db4bf47da54d65aff185b"
+  apiKey:"",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 // Initialize Firebase
